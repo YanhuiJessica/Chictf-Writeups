a12c5e6f5e5f20caca2ab7ef1b977e857a25e9ef
YanhuiJessica <137126578@qq.com>
2020-10-20 00:13:14 +0800

    token更改

--- docs/static/js/extra.js
+++
@@ -1,6 +0,0 @@
-$( document ).ready( function () {
-    $( '.contributors img[data-src]' ).each( function() {
-        src = $(this).attr("data-src");
-        $(this).attr('src',src);
-    });
-} );
